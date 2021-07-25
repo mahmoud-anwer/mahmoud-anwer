de142cab6beeb25ff0efe7118afc6e0d21603954
mahmoud-anwer <mahmoudanwer071@gmail.com>
2021-07-25 02:34:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+### Hi there, I'm Mahmoud Anwer <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="25px">
+[![Website](https://img.shields.io/badge/-Portfolio-black?style=for-the-badge&logo=google-chrome&logoColor=white)](https://www.abd3lraouf.tech/portfolio/)
+[![Mail](https://img.shields.io/badge/-Say%20Hi!-black?style=for-the-badge&logo=gmail)](mailto:abdelraoufsabri@gmail.com)
+[![Twitter](https://img.shields.io/badge/-Twitter-black?style=for-the-badge&logo=twitter)](https://twitter.com/abd3lraouf)
+[![Linkedin](https://img.shields.io/badge/-LinkedIn-black?style=for-the-badge&logo=Linkedin)](https://www.linkedin.com/in/abdelraouf-sabri/)
+<!-- 
+<p align='right'>
+      <a href="https://github.com/AbdElraoufSabri/AbdElraoufSabri/releases/latest/download/AbdElraouf.Sabri.resume.pdf">
+            <img alt="Ansible Role" src="https://img.shields.io/static/v1?color=red&label=Resume&logo=adobe&logoColor=white&style=for-the-badge&message=Download">
+      </a>
+</p>
+ -->
+<p align='right'>
+      <a href="https://github.com/AbdElraoufSabri">
+            <img alt="Ansible Role" src="https://komarev.com/ghpvc/?username=AbdElraoufSabri&label=PROFILE+VIEWS&color=red&style=flat-square">
+      </a>
+</p>
+
+## 🤵 About Me:
+- 🏦 I'm a DevOps Engineer, who wants to cloud everything ❤️❤️).
+      <img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giphy.gif" width="30">
+- 🤔 I use daily `.java`,` .kt`, `.sh`
+- 😊 Pronouns: He/Him
+- 🔑 GPG key: `6CF56D21B9A90B5F`
+- 👯 I’m looking to collaborate with other developers
+- 💬 Got 2+ years of Exp in: **Clean architecture**, **Dependency Injection**, **S.O.L.I.D**, **Android Development**.
+
+<p align="center">
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width="32" height="32"/> 
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/kotlin/kotlin-original.svg" alt="kotlin" width="32" height="32"/> 
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/gradle/gradle-plain.svg" alt="gradle" width="32" height="32"/> 
+</p>
+
+### 🎮 Latest Inventions
+**PS:** I've got other multi-module projects developed, tested and maintained using Clean architecture, TDD and DI. If you are a recruiter just [let me know](mailto:abdelraoufsabri@gmail.com)
+
+<!-- - [MVI posts](https://github.com/AbdElraoufSabri/MVIPosts) --> 
+- [Jetpack compose challenge #1: The Pet Adoption App](https://github.com/AbdElraoufSabri/compose-challenge-1)
+- [Jetpack compose challenge #2: The Countdown Timer App](https://github.com/AbdElraoufSabri/compose-challenge-2)
+- [Twitter Search app](https://github.com/AbdElraoufSabri/WeeTwit)
+- [Open Source Library: RxJava3-Datastore](https://github.com/AbdElraoufSabri/DatastoreWithRxJava3)
+- [Open Source Library: CircularProgressBar](https://github.com/AbdElraoufSabri/CircularProgressBar)
+- [Open Source Library: FancyAndroidRuler](https://github.com/AbdElraoufSabri/FancyAndroidRuler)
+<!-- - [MVI sample](https://github.com/AbdElraoufSabri/mviSample) -->
+
+### 📕 My Presentations:
+
+- [Social Engineering](https://abdelraoufsabri.github.io/Presentation), 26th Apil, 2020
+
+### 📕 My Latest Blog posts:
+<!-- BLOG-POST-LIST:START -->
+<!-- BLOG-POST-LIST:END -->
+
+<!--START_SECTION:waka-->
+
+<!--END_SECTION:waka-->
+
+⏳ **Year Progress** { ████████████████▁▁▁▁▁▁▁▁▁▁▁▁▁▁ } 55.95 % as on ⏰ 24-7-2021.
+
+
---
+++ index.js
@@ -0,0 +1,78 @@
+const thisYear = new Date().getFullYear()
+const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
+const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
+const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
+const progressBarOfThisYear = generateProgressBar()
+
+function generateProgressBar() {
+    const progressBarCapacity = 30
+    const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
+    const progressBar = Array(progressBarCapacity)
+        .fill('▁')
+        .map((value, index) => index < passedProgressBarIndex ? '█' : value)
+        .join('')
+    return `{ ${progressBar} }`
+}
+
+const readme = `\
+### Hi there, I'm AbdElraouf Sabri <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="25px">
+[![Website](https://img.shields.io/badge/-Portfolio-black?style=for-the-badge&logo=google-chrome&logoColor=white)](https://www.abd3lraouf.tech/portfolio/)
+[![Mail](https://img.shields.io/badge/-Say%20Hi!-black?style=for-the-badge&logo=gmail)](mailto:abdelraoufsabri@gmail.com)
+[![Twitter](https://img.shields.io/badge/-Twitter-black?style=for-the-badge&logo=twitter)](https://twitter.com/abd3lraouf)
+[![Linkedin](https://img.shields.io/badge/-LinkedIn-black?style=for-the-badge&logo=Linkedin)](https://www.linkedin.com/in/abdelraouf-sabri/)
+<!-- 
+<p align='right'>
+      <a href="https://github.com/AbdElraoufSabri/AbdElraoufSabri/releases/latest/download/AbdElraouf.Sabri.resume.pdf">
+            <img alt="Ansible Role" src="https://img.shields.io/static/v1?color=red&label=Resume&logo=adobe&logoColor=white&style=for-the-badge&message=Download">
+      </a>
+</p>
+ -->
+<p align='right'>
+      <a href="https://github.com/AbdElraoufSabri">
+            <img alt="Ansible Role" src="https://komarev.com/ghpvc/?username=AbdElraoufSabri&label=PROFILE+VIEWS&color=red&style=flat-square">
+      </a>
+</p>
+
+## 🤵 About Me:
+- 🏦 I'm an Android Developer, who is building awesome projects ❤️❤️).
+      <img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giphy.gif" width="30">
+- 🤔 I use daily \`.java\`,\` .kt\`, \`.sh\`
+- 😊 Pronouns: He/Him
+- 🔑 GPG key: \`6CF56D21B9A90B5F\`
+- 👯 I’m looking to collaborate with other developers
+- 💬 Got 2+ years of Exp in: **Clean architecture**, **Dependency Injection**, **S.O.L.I.D**, **Android Development**.
+
+<p align="center">
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width="32" height="32"/> 
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/kotlin/kotlin-original.svg" alt="kotlin" width="32" height="32"/> 
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/gradle/gradle-plain.svg" alt="gradle" width="32" height="32"/> 
+</p>
+
+### 🎮 Latest Inventions
+**PS:** I've got other multi-module projects developed, tested and maintained using Clean architecture, TDD and DI. If you are a recruiter just [let me know](mailto:abdelraoufsabri@gmail.com)
+
+<!-- - [MVI posts](https://github.com/AbdElraoufSabri/MVIPosts) --> 
+- [Jetpack compose challenge #1: The Pet Adoption App](https://github.com/AbdElraoufSabri/compose-challenge-1)
+- [Jetpack compose challenge #2: The Countdown Timer App](https://github.com/AbdElraoufSabri/compose-challenge-2)
+- [Twitter Search app](https://github.com/AbdElraoufSabri/WeeTwit)
+- [Open Source Library: RxJava3-Datastore](https://github.com/AbdElraoufSabri/DatastoreWithRxJava3)
+- [Open Source Library: CircularProgressBar](https://github.com/AbdElraoufSabri/CircularProgressBar)
+- [Open Source Library: FancyAndroidRuler](https://github.com/AbdElraoufSabri/FancyAndroidRuler)
+<!-- - [MVI sample](https://github.com/AbdElraoufSabri/mviSample) -->
+
+### 📕 My Presentations:
+
+- [Social Engineering](https://abdelraoufsabri.github.io/Presentation), 26th Apil, 2020
+
+### 📕 My Latest Blog posts:
+<!-- BLOG-POST-LIST:START -->
+<!-- BLOG-POST-LIST:END -->
+
+<!--START_SECTION:waka-->
+
+<!--END_SECTION:waka-->
+
+⏳ **Year Progress** ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} % as on ⏰ ${new Date().getDate()+'-'+(new Date().getMonth()+1)+'-'+new Date().getFullYear()+'.'}
+
+`
+console.log(readme)
